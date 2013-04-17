d60ca097a0ebe34a7bb1eaaef647bf823f597916
shiyj <shiyj.cn@gmail.com>
2013-04-17 23:24:15 +0800

    transfer blog to jekyll

---
+++ _plugins/blogtags_ext.rb
@@ -0,0 +1,17 @@
+require 'jekyll/tagging'
+module Jekyll
+  class BlogTagsPage < Page
+    def initialize(site, base, dir, tag)
+    end
+  end
+
+  class BlogTagsPageGenerator < Generator
+    safe true
+
+    def generate site
+
+    end
+
+  end
+
+end
---
+++ _plugins/markdown_ext.rb
@@ -0,0 +1,26 @@
+require 'redcarpet'
+module Jekyll
+  class MarkdownExt < Converter
+
+    def matches(ext)
+      ext =~ /toto/i
+    end
+
+    def output_ext(ext)
+      '.html'
+    end
+
+    def convert(content)
+      mark = Redcarpet::Markdown.new(Redcarpet::Render::HTMLwithTopic.new, {
+        :autolink => true,
+        :fenced_code_blocks => true,
+        :strikethrough => true,
+        :space_after_headers => true,
+        :no_intra_emphasis => true
+      })
+      content.gsub!("```","\n```")
+      mark.render(content)
+    end
+
+  end
+end
