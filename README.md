d50946fe84a194873daf61d0846a53a43133a907
shiyj <shiyj.cn@gmail.com>
2014-01-27 09:23:13 +0800

    init pages

---
+++ README.md
@@ -0,0 +1,6 @@
+shiyj-jekyll
+============
+
+my new blog transformed from toto
+
+you can find the website [here](http://blog.geoinker.com)
---
+++ _plugins/blogtags_ext.rb
@@ -0,0 +1,19 @@
+# encoding: UTF-8
+
+require 'jekyll/tagging'
+module Jekyll
+  class BlogTagsPage < Page
+    def initialize(site, base, dir, tag)
+    end
+  end
+
+  class BlogTagsPageGenerator < Generator
+    safe true
+
+    def generate site
+
+    end
+
+  end
+
+end
